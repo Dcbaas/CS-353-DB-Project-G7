c6659bae5369e9d0f858418a6d8f06e50da93333
Dcbaas <Dcbaas@users.noreply.github.com>
2019-04-02 20:09:41 -0400

    init commit

---
+++ createDB.sql
@@ -0,0 +1,12 @@
+CREATE TABLE car(
+    serialNum INTEGER PRIMARY KEY,
+    carModel  CHAR(20),
+    carType   CHAR(20),
+    carRate   INTEGER,
+    color     CHAR(15),
+    licecePlateNum CHAR(8),
+    year      INTEGER, 
+    bAddress  CHAR(15)
+
+
+);
