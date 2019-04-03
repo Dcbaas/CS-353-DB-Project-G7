ece9b6e8295e370e8c0c6fbbcf5489c3c217fc81
Dcbaas <Dcbaas@users.noreply.github.com>
2019-04-02 20:26:39 -0400

    finished inital table creation

--- createDB.sql
+++ createDB.sql
@@ -6,7 +6,67 @@ CREATE TABLE car(
     color     CHAR(15),
     licecePlateNum CHAR(8),
     year      INTEGER, 
-    bAddress  CHAR(15)
+    bID       INTEGER
+    -- Make constraints later
 
-
+);
+--
+CREATE TABLE branch(
+    bID INTEGER PRIMARY KEY,
+    bAddress CHAR(20),
+    lotSize INTEGER,
+    managerSSN INTEGER,
+    managerStartDate DATE
+);
+--
+CREATE TABLE employee(
+    eSSN      INTEGER PRIMARY KEY,
+    name      CHAR(20),
+    startDate DATE,
+);
+--
+CREATE TABLE customer(
+    licenseID   INTEGER PRIMARY KEY,
+    insurance   CHAR(20),
+    billingAddr CHAR(30),
+    age         INTEGER,
+    name        CHAR(20),
+    consultantSSN INTEGER
+);
+--
+CREATE TABLE rental(
+    orderID INTEGER,
+    cost    CHAR(15),
+    startDate DATE,
+    returnDate DATE,
+    milesUsed INTEGER, 
+    renterLicenseID INTEGER,
+    bID      INTEGER,
+    carSerialNum INTEGER
+);
+--
+CREATE TABLE repair_shop(
+    RsID INTEGER,
+    garageSpaces INTEGER, 
+    rAddress CHAR(15)
+);
+--
+CREATE TABLE repair_history(
+    carSerialNum INTEGER,
+    year        INTEGER,
+    cost        INTEGER,
+    numServices INTEGER
+);
+--
+CREATE TABLE languages(
+    eSSN INTEGER,
+    language CHAR(15)
+);
+--
+CREATE TABLE service(
+    rSid    INTEGER,
+    carSerialNum INTEGER, 
+    sDate   DATE,
+    cost    INTEGER,
+    reason  CHAR(40)
 );
